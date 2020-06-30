2ce0f99d586fdf6b598a77f2db305b03a81d1449
satyanarayan <pardhisatyanarayan@gmail.com>
2020-06-30 11:21:44 +0530

    post to api

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ app = FastAPI()
 async def index():
     return {"text": "Hello api master"}
 
+@app.get('/items/{name}')
+async def get_items(name):
+    return {"name": name}
+
+@app.post('/items/{name}')
+async def get_items(name):
+    return {"name": name}
+
 if __name__ == '__main__':
     uvicorn.run(app,host = "127.0.0.1",port = 8000)
